1a26e9b948e1bde12851ec19d3e96cdfde7ddd0c
pallavigaikwad24 <50769683+pallavigaikwad24@users.noreply.github.com>
2023-06-23 09:18:43 +0530

    Text Editor Program

---
+++ TextEditor/src/task_five/Main.java
@@ -0,0 +1,21 @@
+package task_five;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TextEditor textEditor = new TextEditor();
+        textEditor.setTitle("Text Editor");
+        textEditor.setSize(800, 600);
+        textEditor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textEditor.setVisible(true);
+
+	}
+
+}
